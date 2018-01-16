dbd36d5efdad1ca158c7ffdbfa3b81453cc8c3a6
Damon Tung <guangzhen.dong@163.com>
2018-01-16 10:24:19 +0800

    dict 修改

--- FTP_SFTP_Client.py
+++ FTP_SFTP_Client.py
@@ -3,6 +3,8 @@ import Config
 import os
 import time
 import pysftp
+import shutil
+import myLogger as log
 
 
 class FTPClient:
@@ -14,6 +16,7 @@ class FTPClient:
         self.ftp = FTP()
         self.ftp.set_debuglevel(2)
         self.local_dir = self.config.local_env()
+        self.logging = log.MyLogger()
         if os.path.exists(self.local_dir):
             os.chdir(self.local_dir)
         else:
@@ -34,15 +37,18 @@ class FTPClient:
         self.ftp.cwd(dir)
         self.ftp.nlst()
         file_handle = open(self.filename, 'wb')
+        self.logging.info("Jenkins Server: " + hosts + " |dir: " + dir)
+        self.logging.info("start downloading...")
         print ("start downloading...")
         self.ftp.retrbinary("RETR " + self.filename, file_handle.write, 1024)
         self.ftp.set_debuglevel(0)
         # self.ftp.close()
         self.ftp.quit()
+        self.logging.info("download complete ...")
+        self.logging.info("Current Path: " + self.local_dir)
         print("download complete...")
         print("path: " + self.local_dir)
         # shutil.copy2(self.filename, self.filename_local)
-
 
     def upload_to_201(self):
         mc_201 = self.config.host_201()
@@ -53,6 +59,7 @@ class FTPClient:
         remote_dir = mc_201['remote_dir']
         print(mc_201)
         self.upload(hosts, username, password, port, remote_dir)
+        self.logging.info("Update 201: " + hosts + " |Dir: " + remote_dir)
 
     def upload_to_202(self):
         mc_202 = self.config.host_202()
@@ -63,6 +70,7 @@ class FTPClient:
         remote_dir = mc_202['remote_dir']
         print(mc_202)
         self.upload(hosts, username, password, port, remote_dir)
+        self.logging.info("Update 202: " + hosts + " |Dir: " + remote_dir)
 
     def upload_to_57(self):
         mc_57 = self.config.host_57()
@@ -73,6 +81,7 @@ class FTPClient:
         remote_dir = mc_57['remote_dir']
         print(mc_57)
         self.upload(hosts, username, password, port, remote_dir)
+        self.logging.info("Update 57: " + hosts + " |Dir: " + remote_dir)
 
     def rename_war(self):
         if os.path.isfile(self.filename):
@@ -80,11 +89,23 @@ class FTPClient:
                 os.remove(self.filename_local)
             os.rename(self.filename, self.filename_local)
             print("copy " + self.filename + "  to " + self.filename_local)
+            self.logging.info("copy " + self.filename + "  to " + self.filename_local)
 
     def upload(self, hosts, username, password, port, remote_dir):
         # self.rename_war()
         with pysftp.Connection(host=hosts, username=username, password=password, port=port, default_path=remote_dir) as sftp:
             print("current path: " + sftp.pwd)
+            self.logging.info("upload start ...")
             sftp.put(self.filename_local)
+            self.logging.info("upload complete...")
             print("上传成功!")
 
+    def copy_to_backup(self):
+        local_time = time.strftime('%Y-%m-%d %H-%M', time.localtime(time.time()))
+        os.chdir(self.local_dir)
+        os.chdir('../')
+        if not os.path.exists(local_time):
+            os.mkdir(local_time)
+            shutil.copy2('./uat/menuCenter.war', local_time + '/menuCenter.war')
+            print("war 包备份至: " + os.getcwd() + "\\" + local_time)
+            self.logging.info("war 包备份至: " + os.getcwd() + "\\" + local_time)
--- start.py
+++ start.py
@@ -1,10 +1,15 @@
 import FTP_SFTP_Client
+import myLogger
 
 ftp = FTP_SFTP_Client.FTPClient()
 print("download war from jenkins server ...")
 ftp.conn_jenkins()
 print("重命名 WAR 包.")
 ftp.rename_war()
+print("备份 war 包")
+ftp.copy_to_backup()
+
+myLog = myLogger.MyLogger().mylogging()
 update_uat = input("\nupdate uat: [y/n]: ")
 
 if update_uat == 'y':
@@ -12,15 +17,19 @@ if update_uat == 'y':
     update_57 = input("update 57 ? [y/n]: ")
     if update_57 == 'y':
         ftp.upload_to_57()
+        myLog.info("57 updated.")
         print("57 updated")
     update_201 = input("update 201? [y/n]: ")
     if update_201 == 'y':
         ftp.upload_to_201()
+        myLog.info("201 updated.")
         print("201 updated")
     update_202 = input("update 202 ?[y/n]: ")
     if update_202 == 'y':
         ftp.upload_to_202()
+        myLog.info("202 updated.")
         print("202 updated.")
 else:
+    myLog.log("no update")
     print("no update.")
 
