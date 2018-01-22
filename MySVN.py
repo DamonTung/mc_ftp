67ba62c8fd0543a9c5c2f0690b14080ac87b8937
Damon Tung <guangzhen.dong@163.com>
2018-01-22 11:31:24 +0800

    add callback_get_login

--- MySVN.py
+++ MySVN.py
@@ -11,6 +11,20 @@ class McSVN:
         self.client = pysvn.Client()
         self.logging = myLogger.MyLogger().mylogging()
 
+        def callback_get_login(realm, username, may_save):
+            name = 'dongguangzhen'
+            password = 'dongguangzhen'
+            return True, name, password, True
+
+        self.client.callback_get_login = callback_get_login
+
+    def get_login(realm, username, may_save):
+        retcode = True
+        username = 'dongguangzhen'
+        password = 'dongguangzhen'
+        save = True
+        return retcode, username, password, save
+
     def check_out(self):
         self.logging.info("check out from {} to {} ".format(self.mc_web_remote, self.mc_web_local))
         self.client.checkout(self.mc_web_remote, self.mc_web_local)
@@ -24,6 +38,11 @@ class McSVN:
         self.logging.info("updated.")
         headrev_after = self.client.info(self.mc_web_local).revision.number
         self.logging.info("updated Head Revision: " + str(headrev_after))
+        self.logging.info("updated records: " )
+        self.logging.info(self.client.diff_summarize(
+            url_or_path1=self.mc_web_remote, revision1=pysvn.Revision(pysvn.opt_revision_kind.number, headrev_before),
+            url_or_path2=self.mc_web_remote, revision2=pysvn.Revision(pysvn.opt_revision_kind.number, headrev_after),
+            recurse=True, ignore_ancestry=False))
 
     def get_changes(self):
         changes = self.client.status(self.mc_web_local)
@@ -37,5 +56,7 @@ class McSVN:
         self.logging.info([f.path for f in changes if f.text_status == pysvn.wc_status_kind.conflicted])
         self.logging.info('unversioned files:')
         self.logging.info([f.path for f in changes if f.text_status == pysvn.wc_status_kind.unversioned])
+
+
 
 
